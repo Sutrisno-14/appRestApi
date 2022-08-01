f7489dc8e2968e7f212e1ce85f113896c16f10d6
Sutrisno14 <masutrisn021@gmail.com>
2022-08-01 14:54:36 +0700

    Add File controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-    response.ok('Aplikasi success running');
-}
+    response.ok('Aplikasi success running')
+};
